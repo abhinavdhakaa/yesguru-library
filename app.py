cffb816065234c790a9cf468880869fe8d444705
Abhinav Dhaka <AbhinavDhaaka@gmail.com>
2025-06-09 19:53:26 +0530

    updatee app last port policyrazorpay  update 5  new

--- app.py
+++ app.py
@@ -164,13 +164,9 @@ def refund():
 def pricing():
     return render_template('pricing.html')
 
-
 @app.route('/shipping')
 def shipping():
     return render_template('shipping.html')
-@app.route('/about')
-def about():
-    return render_template('about.html')
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5500)
